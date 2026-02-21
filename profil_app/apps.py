9d17432643ecde167e0e91cddbc3c151f0e079e0
alexschoefer <alex.schoefer@gmx.net>
2026-02-21 12:34:03 +0100

    feat: add profil_app with the models customer and business profil

---
+++ profil_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProfilAppConfig(AppConfig):
+    name = 'profil_app'
---
+++ profil_app/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Imports all settings from core
+from django.conf import settings
+
+class CustomerProfile(models.Model):
+    """
+    Profile for the customer user
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="customer_profile"
+    )
+
+    def __str__(self):
+        """
+        Return a string of the customer profile.
+
+        Returns:
+            str: The full name of the customer.
+        """
+        return f"{self.username}"
+
+class BusinessProfil(models.Model):
+    """
+    Profile for the business user with business specific additional fields
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="business_profile"
+    )
+
+    tel = models.CharField(max_length=20, blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    description = models.TextField(max_length=255,blank=True)
+    working_hours = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        """
+        Return a string of the business profile.
+
+        Returns:
+            str: The full name of the business user.
+        """
+        return f"{self.username}"
