04f6504e068e8404368b688a3699c12faf793971
alexschoefer <alex.schoefer@gmx.net>
2026-03-17 20:41:00 +0100

    feat: register apps base_info, orders, profil, reviews and upload

--- upload_app/models.py
+++ upload_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class FileUpload(models.Model):
+    """Model to represent a file upload."""
     file = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """String representation of the FileUpload model, showing the file name."""
         return self.file.name
