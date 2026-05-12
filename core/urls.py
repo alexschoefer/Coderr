3e43bc6b24026e5bf487288c2550eb495d13ac68
alexschoefer <alex.schoefer@gmx.net>
2026-05-12 22:14:25 +0200

    update: urls.py for media_url

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('', include('orders_app.api.urls')),
     path('', include('reviews_app.api.urls')),
     path('', include('base_info_app.api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
