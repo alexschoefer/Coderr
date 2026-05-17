b5ec940fb6c71a7fade45e5b056b323830a76a47
alexschoefer <alex.schoefer@gmx.net>
2026-05-17 15:54:11 +0200

    feat: update urls in app orders, profil und review

--- reviews_app/api/urls.py
+++ reviews_app/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ReviewListCreateAPIView, ReviewRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
-    path('api/reviews/', ReviewListCreateAPIView.as_view(), name='review-list-create'),
-    path('api/reviews/<int:pk>/', ReviewRetrieveUpdateDestroyAPIView.as_view(), name='review-detail'),
+    path('reviews/', ReviewListCreateAPIView.as_view(), name='review-list-create'),
+    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyAPIView.as_view(), name='review-detail'),
 ]
